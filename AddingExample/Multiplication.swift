df68d0e38cfa4e0c26bdb559905e9636b3538b88
Nina Zhou <nzhou@lcs.on.ca>
2023-11-06 12:10:12 -0500

    created multiplication tab

---
+++ AddingExample/Multiplication.swift
@@ -0,0 +1,18 @@
+//
+//  Multiplication.swift
+//  AddingExample
+//
+//  Created by Shuyu Zhou on 2023-11-06.
+//
+
+import SwiftUI
+
+struct Multiplication: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Multiplication()
+}
