280887ad41163a133c3485aead62d0a4e4efa782
Nina Zhou <nzhou@lcs.on.ca>
2023-11-06 11:20:26 -0500

    Initial Commit

---
+++ AddingExample/AddingExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  AddingExampleApp.swift
+//  AddingExample
+//
+//  Created by Shuyu Zhou on 2023-11-06.
+//
+
+import SwiftUI
+
+@main
+struct AddingExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
